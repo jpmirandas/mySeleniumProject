ad376d59735341ac4f0e766cda78e1670dd5bb0c
João Paulo Miranda <jpmirandas@gmail.com>
2017-07-05 17:52:31 -0300

    Add the class that reads the property file

---
+++ src/util/PropertyReader.java
@@ -0,0 +1,24 @@
+package util;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyReader {
+
+	public static String getProperty(String propertyName) {
+		Properties property = new Properties();
+		
+		try {
+			property.load(new FileInputStream("./properties/selenium.properties"));
+		} catch (FileNotFoundException e) {
+			System.err.println("The file selenium.properties was not found!");			
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("IO problem when trying to read the selenium.properties file!");
+			e.printStackTrace();
+		}
+		return property.getProperty(propertyName);
+	}
+}
